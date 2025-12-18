a0ca076518b1100e156872bb489b8ec045a0a449
K4rl1Hub <michael@dr-karlstetter.de>
2025-12-18 21:27:18 +0100

    fiy type

--- custom_components/warema_webcontrol/__init__.py
+++ custom_components/warema_webcontrol/__init__.py
@@ -59,7 +59,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data[DOMAIN]["coordinator"] = coordinator
 
     # Plattformen laden
-    await hass.config_entries.sync_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
